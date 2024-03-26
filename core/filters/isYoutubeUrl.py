3966a5d0b065aabaddcf5ae7476c517e472bb9ea
lap1as <kuzukoleg12@gmail.com>
2024-03-26 20:38:02 +0100

    fix filter to get mobile video links

--- core/filters/isYoutubeUrl.py
+++ core/filters/isYoutubeUrl.py
@@ -7,7 +7,7 @@ class IsYoutubeUrl(BaseFilter):
     async def __call__(self, message: Message) -> bool:
         try:
             url = message.text
-            match = re.match(r'(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)[\w-]+(&\S*)?$', url)
+            match = re.match(r'(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)?[\w-]{11}(?=\S*[&\?]si=[^&$]+)|(https?://)?(www\.)?youtube\.com/watch\?v=[\w-]{11}&si=[^&$]+', url, re.IGNORECASE)
             if match:
 
                 return True
