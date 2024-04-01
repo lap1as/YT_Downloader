d22b474de97652b9ec902290627d445f11a45671
lap1as <kuzukoleg12@gmail.com>
2024-04-01 03:48:50 +0200

    fix filter to get desktop video links

--- core/filters/isYoutubeUrl.py
+++ core/filters/isYoutubeUrl.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 import re
@@ -7,9 +9,11 @@ class IsYoutubeUrl(BaseFilter):
     async def __call__(self, message: Message) -> bool:
         try:
             url = message.text
-            match = re.match(r'(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)?[\w-]{11}(?=\S*[&\?]si=[^&$]+)|(https?://)?(www\.)?youtube\.com/watch\?v=[\w-]{11}&si=[^&$]+', url, re.IGNORECASE)
-            if match:
-
+            match_mobile_link = re.match(r'(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)?[\w-]{11}(?=\S*[&\?]si=[^&$]+)|(https?://)?(www\.)?youtube\.com/watch\?v=[\w-]{11}&si=[^&$]+', url, re.IGNORECASE)
+            match_desktop_link = re.match(r'(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)[\w-]+(&\S*)?$', url)
+            if match_mobile_link or match_desktop_link:
                 return True
-        except:
-            return False
+            else:
+                return False
+        except Exception as e:
+            logging.error(e)
